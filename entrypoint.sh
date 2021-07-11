996a8a0dae628ec3a6a9a81f67ca14678f144b77
Ichinose Shogo <shogo82148@gmail.com>
2021-07-12 07:29:23 +0900

    add level, reporter, filter_mode, fail_on_error, and reviewdog_flags options

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,12 @@
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-actionlint -oneline | reviewdog -efm="%f:%l:%c: %m"
+actionlint -oneline \
+    | reviewdog \
+        -efm="%f:%l:%c: %m" \
+        -name="actionlint" \
+        -reporter="${INPUT_REPORTER}" \
+        -filter-mode="${INPUT_FILTER_MODE}" \
+        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+        -level="${INPUT_LEVEL}" \
+        ${INPUT_REVIEWDOG_FLAGS}
