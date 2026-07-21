5cda17225ded9e9eae9f58f58bc7728f3743b463
ICHINOSE Shogo <shogo82148@gmail.com>
2026-07-21 23:52:07 +0900

    feat: install pyflakes in Dockerfile and add installation script

---
+++ scripts/install-pyflakes.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+
+if [ -n "${RUNNER_DEBUG:-}" ] ; then
+  set -x
+fi
+
+cd "$(dirname "$0")"
+python3 -m pip install --no-cache-dir --upgrade pip
+python3 -m pip install --no-cache-dir -r ./requirements.txt
