3a08a20299d9f386704f11eeb4d4cb491284b9cc
ICHINOSE Shogo <shogo82148@gmail.com>
2026-07-22 00:12:47 +0900

    feat: move virtual environment setup inside OS check in install-pyflakes script

--- scripts/install-pyflakes.sh
+++ scripts/install-pyflakes.sh
@@ -6,7 +6,12 @@ if [ -n "${RUNNER_DEBUG:-}" ] ; then
 fi
 
 cd "$(dirname "$0")"
-python3 -m venv venv
-. venv/bin/activate
+
+OS_NAME="$(uname -s | tr '[:upper:]' '[:lower:]')"
+if [ "${OS_NAME}" = "darwin" ]; then
+  python3 -m venv venv
+  . venv/bin/activate
+fi
+
 python3 -m pip install --no-cache-dir --upgrade pip
 python3 -m pip install --no-cache-dir -r ./requirements.txt
