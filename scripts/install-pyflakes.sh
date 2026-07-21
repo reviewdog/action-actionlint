f254e5d05b12fdaf1590be977940474a95802f75
ICHINOSE Shogo <shogo82148@gmail.com>
2026-07-22 06:19:28 +0900

    fix: streamline pyflakes installation for macOS using pipx

--- scripts/install-pyflakes.sh
+++ scripts/install-pyflakes.sh
@@ -9,9 +9,8 @@ cd "$(dirname "$0")"
 
 OS_NAME="$(uname -s | tr '[:upper:]' '[:lower:]')"
 if [ "${OS_NAME}" = "darwin" ]; then
-  python3 -m venv venv
-  . venv/bin/activate
+  pipx install pyflakes
+else
+  python3 -m pip install --no-cache-dir --upgrade pip
+  python3 -m pip install --no-cache-dir -r ./requirements.txt
 fi
-
-python3 -m pip install --no-cache-dir --upgrade pip
-python3 -m pip install --no-cache-dir -r ./requirements.txt
