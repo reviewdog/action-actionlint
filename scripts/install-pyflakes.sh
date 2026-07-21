3756c968b5cc838a813d2f59e7c4d54328985b25
ICHINOSE Shogo <shogo82148@gmail.com>
2026-07-22 00:09:26 +0900

    feat: add virtual environment setup to install-pyflakes script

--- scripts/install-pyflakes.sh
+++ scripts/install-pyflakes.sh
@@ -6,5 +6,7 @@ if [ -n "${RUNNER_DEBUG:-}" ] ; then
 fi
 
 cd "$(dirname "$0")"
+python3 -m venv venv
+. venv/bin/activate
 python3 -m pip install --no-cache-dir --upgrade pip
 python3 -m pip install --no-cache-dir -r ./requirements.txt
